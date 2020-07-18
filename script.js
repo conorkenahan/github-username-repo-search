f32af1f50bb3469d283720bb4d8c0e670db93a4e
Conor <67439130+conorkenahan@users.noreply.github.com>
2020-07-18 13:46:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const apiKey = "1ef5b2dcbfddc0212ba609031f9d971cd2bdb99e";
+
+
+
+function searchUser(username){
+  fetch(`https://api.github.com/users/${username}/repos`)
+  .then(res => res.json())
+  .then(repos => displayResults(repos))
+  .catch(error => 'Something went wrong, sorry :(');
+}
+
+function displayResults(repos){
+  $('.results').removeClass('hidden');
+  $('.results').html('');
+  repos.forEach(repo=>{
+    $('.results').append(`
+      <ul><li><a href="${repo.url}" target="_blank">${repo.name}</a></li></ul>
+    `)
+  });
+  
+}
+
+
+
+function clickSubmit(){
+  $('#submit').submit(event => {
+    event.preventDefault();
+    const username = $('#username').val();
+    searchUser(username);
+  })
+}
+
+
+
+
+$(function(){
+  clickSubmit();
+});
